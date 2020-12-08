1de1a337687cec6bca8e7ce4ed4c9a382788814b
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-12-08 17:13:24 -0500

    this is a commit - be warned

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,45 @@
+use crate::read_nums;
+use std::collections::HashSet;
+
+fn find_product() -> Option<isize> {
+    let mut prev_nums: HashSet<isize> = HashSet::new();
+    let nums = read_nums("resources/day1.txt").unwrap();
+    for num in nums {
+        let complement = 2020 - num;
+        if prev_nums.contains(&complement) {
+            return Some(num * complement);
+        }
+        prev_nums.insert(num);
+    }
+    None
+}
+
+fn find_product2() -> Option<isize> {
+    let mut prev_nums: HashSet<isize> = HashSet::new();
+    let nums = read_nums("resources/day1.txt").unwrap();
+    nums.retain(|&x| x
+    nums.iter()
+        
+    for num in nums {
+        let complement = 2020 - num;
+        if prev_nums.contains(&complement) {
+            return Some(num * complement);
+        }
+        prev_nums.insert(num);
+    }
+    None
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn run_task1() {}
+    fn run_task1() {
+        let x = find_product();
+        dbg!(x);
+        assert_eq!(true, true);
+    }
 
     #[test]
     fn run_task2() {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
+use anyhow::Result;
 use std::fs::File;
 use std::io::prelude::*;
 use std::num::ParseIntError;
-use anyhow::Result;
-
 
 pub fn read_nums(filename: &str) -> Result<Vec<isize>> {
     let mut file = File::open(filename)?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
-    let iter = input.split_whitespace();
-    let x : std::result::Result<Vec<isize>, ParseIntError> = iter.map(|x| x.parse::<isize>()).collect();
-    Ok(x?) 
+    let nums: std::result::Result<Vec<isize>, ParseIntError> = input
+        .split_whitespace()
+        .map(|x| x.parse::<isize>())
+        .collect();
+
+    Ok(nums?)
 }
 
 pub mod day1;
